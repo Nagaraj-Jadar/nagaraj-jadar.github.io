9e4a53d713d2ff58e8a956d36dbbb27c0b07c1e3
Nagaraj Jadar <nagarajbjadar@gmail.com>
2025-03-07 00:37:56 +0530

    Updated Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
+  strings : ["Developer","Recent MCA Graduate","Aspiring Software Engineer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
